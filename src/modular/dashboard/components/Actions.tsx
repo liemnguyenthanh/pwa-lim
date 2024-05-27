8631d6b2a1804bf11000f7810ff6b18d3196a6e9
liemnt <liemdonduong@gmail.com>
2024-05-27 14:12:05 +0700

    chore: login

--- src/modular/dashboard/components/Actions.tsx
+++ src/modular/dashboard/components/Actions.tsx
@@ -1,12 +1,10 @@
 import { usePrivy } from "@privy-io/react-auth";
 import React from "react";
-import type {
-  UnsignedTransactionRequest,
-} from "@privy-io/react-auth";
+import type { SendTransactionModalUIOptions, UnsignedTransactionRequest } from "@privy-io/react-auth";
 import { Button } from "flowbite-react";
 
-export const Transaction = () => {
-  const { sendTransaction } = usePrivy();
+export const Actions = () => {
+  const { sendTransaction, getAccessToken } = usePrivy();
 
   const requestData: UnsignedTransactionRequest = {
     to: "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045",
@@ -14,14 +12,24 @@ export const Transaction = () => {
     value: "0x3B9ACA00",
   };
 
-  const uiConfig: any = {
+  const uiConfig: SendTransactionModalUIOptions = {
     header: "Sample header text",
     description: "Sample description text",
     buttonText: "Sample button text",
   };
 
+  const handleVerify = async () => {
+    try {
+      const apiUrl = "http://localhost:8080";
+      const token = await getAccessToken();
+      const loginUrl = apiUrl + "/login?token=" + token;
+      const res = await fetch(loginUrl);
+      console.log("res", res);
+    } catch (error) {}
+  };
+
   return (
-    <div>
+    <div className="flex flex-col gap-4">
       {" "}
       <Button
         onClick={async () => {
@@ -31,6 +39,7 @@ export const Transaction = () => {
       >
         Send ETH
       </Button>
+      <Button onClick={handleVerify}>Verify Authencation</Button>
     </div>
   );
 };
